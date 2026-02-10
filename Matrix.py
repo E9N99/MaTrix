99ff12a7ea540b873ca38dc07d7cbae68273a085
BiLaL <97440505+E9N99@users.noreply.github.com>
2026-02-10 22:43:00 +0300

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import base64
+
+
+repo_encoded = "aHR0cHM6Ly9nY29tjskwmskxFsaTQxNGE0NGFsaS9tYXRyaXg="
+branch = "maen"
+
+def run(cmd):
+    print(f"⌭ تنفيذ: {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+def _run_git_clone():
+    print("• جـاري تحميل سورس ماتركـس.....")
+    repo_url = base64.b64decode(repo_encoded.replace(" ", "")).decode()
+    run(f"git clone -b {branch} {repo_url} source_temp")
+    os.chdir("source_temp")
+
+def _install_requirements():
+    print("⌭ تثبيت مكاتب ماتركـس ⌭")
+    run("pip install -r requirements.txt")
+
+def _start_project():
+    print("⌭ البدء بتشغيل ماتركـس  ⌭")
+    # تشغيل server.py في الخلفية ثم 
+    run("python3 server.py &")
+    run("python3 -m Matrix")
+
+if __name__ == "__main__":
+    _run_git_clone()
+    _install_requirements()
+    _start_project()
